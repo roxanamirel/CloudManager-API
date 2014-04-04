613b81cd82fc37dda640ed766bb89b3a2acd7492
roxyroxy <rroxanaioana@gmail.com>
2014-04-04 11:48:25 +0300

    exception added

--- src/services/Service.java
+++ src/services/Service.java
@@ -2,6 +2,8 @@ package services;
 
 import java.util.List;
 
+import exceptions.ServiceCenterAccessException;
+
 import util.ResponseMessage;
 
 /**
@@ -14,18 +16,18 @@ public abstract class Service<T> {
 	/**Returns element by id
 	 * @return T - element returned by id
 	 */
-	public abstract T getById(int id);
+	public abstract T getById(int id) throws ServiceCenterAccessException;
 	
 	/**Returns all T elements
 	 * @return List<T> - list containing all T elements
 	 */
-	public abstract List<T> getAll();
+	public abstract List<T> getAll() throws ServiceCenterAccessException ;
 	
 	/**Deletes element T
 	 * @param t - element to delete
 	 * @return response message from the cloud manager 
 	 */
-	public abstract ResponseMessage delete(T t);
+	public abstract ResponseMessage delete(T t) throws ServiceCenterAccessException ;
 	
 	/** Checks if element t is contained
 	 * @param t - element to check if contained
